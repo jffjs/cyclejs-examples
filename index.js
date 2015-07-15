e3a383010fd4085d96bc7e4e853a28ef5b43b63d
jffjs <jffreyjs@gmail.com>
2015-07-15 15:33:42 -0400

    simple router

--- index.js
+++ index.js
@@ -2,48 +2,41 @@ import Cycle from '@cycle/core';
 import {makeDOMDriver, h} from '@cycle/web';
 
 var views = {
-  '/1': function() {
-    return h('h2', 'View 1');
+  '/1': {
+    model: function() {
+      return Cycle.Rx.Observable.interval(1000).startWith('');
+    },
+    view: function(state$) {
+      return state$.map(i => h('h2', '' + i));
+    }
   },
-  // '/1': {
-  //   view: (state) => {
-  //     return h('h2', ``);
-  //   },
-  //   model: () => Cycle.Rx.Observable.interval(1000)
-  // },
-  '/2': function() {
-    return h('h2', 'View 2');
+  '/2':{
+    view: function() {
+      return Cycle.Rx.Observable.just(h('h2', 'View 2'));
+    }
+  },
+  '/3': {
+    view: function() {
+      return Cycle.Rx.Observable.just(h('h2', 'View 3'));
+    }
   }
 };
 
-// router.route('/1', {
-//   model: function() {},
-//   view: function() {},
-//   intent: function() {}
-// });
+function getView(route) {
+  let view = views[route] || { view: () => Cycle.Rx.Observable.just(h('h2', 'not found')) };
+  // let intent = view.intent ? view.intent() : null;
+  let model$ = view.model ? view.model() : null;
+  let view$ = view.view(model$);
 
-function view(r) {
-  let route = views[r];
-  let intent = route.intent ? route.intent() : null;
-  let model = route.model ? route.model(intent) : null;
-  let view = route.view ? route.view(model) : null;
-
-  return view;
+  return view$;
 }
+
 function main(responses) {
   let route$ = Cycle.Rx.Observable.fromEvent(window, 'hashchange')
         .map(ev => ev.newURL.match(/\#[^\#]*$/)[0].replace('#', ''))
         .startWith(window.location.hash.replace('#', ''));
 
-  let vtree$ = route$.map(route =>
-    h('div#main',[
-      h('ul', [
-        h('li', h('a', {className: 'nav-link', href: '#/1'}, 'Nav 1')),
-        h('li', h('a', {className: 'nav-link', href: '#/2'}, 'Nav 2')),
-        h('li', h('a', {className: 'nav-link', href: '#/3'}, 'Nav 3'))
-      ]),
-      views[route] ? views[route]() : null
-    ]));
+  let vtree$ = route$.flatMapLatest(route => getView(route));
 
   return {
     DOM: vtree$
