aa9a8d074f45f5e1b2b80888cec0ff7342a81bca
Jeff Smith <jffreyjs@gmail.com>
2015-07-15 21:48:45 -0400

    messing with input

--- index.js
+++ index.js
@@ -11,8 +11,17 @@ var views = {
     }
   },
   '/2':{
-    view: function() {
-      return Cycle.Rx.Observable.just(h('h2', 'View 2'));
+    model: function(input$) {
+      return input$.map(val => val.toUpperCase()).startWith('');
+    },
+    view: function(model$) {
+      return model$.map(name =>
+                        h('div', [
+                          h('input#name'),
+                          h('h2', name)]));
+    },
+    intent: function(DOM) {
+      return DOM.get('input#name', 'input').map(ev => ev.target.value);
     }
   },
   '/3': {
@@ -22,21 +31,21 @@ var views = {
   }
 };
 
-function getView(route) {
+function getView(route, DOM) {
   let view = views[route] || { view: () => Cycle.Rx.Observable.just(h('h2', 'not found')) };
-  // let intent = view.intent ? view.intent() : null;
-  let model$ = view.model ? view.model() : null;
+  let intent$ = view.intent ? view.intent(DOM) : null;
+  let model$ = view.model ? view.model(intent$) : null;
   let view$ = view.view(model$);
 
   return view$;
 }
 
-function main(responses) {
+function main({DOM}) {
   let route$ = Cycle.Rx.Observable.fromEvent(window, 'hashchange')
         .map(ev => ev.newURL.match(/\#[^\#]*$/)[0].replace('#', ''))
         .startWith(window.location.hash.replace('#', ''));
 
-  let vtree$ = route$.flatMapLatest(route => getView(route));
+  let vtree$ = route$.flatMapLatest(route => getView(route, DOM));
 
   return {
     DOM: vtree$
